35c3f9734fd0e4d575b5e082b7ad8b37ed273734
Frikky <frikky@Frikkys-MacBook-Pro.local>
2023-07-25 16:32:47 +0200

    Added Daily stats tracker under /admin?tab=billing to give more insight

--- dashboards.go
+++ dashboards.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"sort"
 
 	"encoding/json"
 	"io/ioutil"
@@ -341,6 +342,33 @@ func HandleGetStatistics(resp http.ResponseWriter, request *http.Request) {
 		resp.WriteHeader(401)
 		resp.Write([]byte(`{"success": false, "reason": "Failed getting stats for your org. Maybe not initialized yet?"}`))
 		return
+	}
+
+	// Should remove the FIRST day as it's very scewed
+	// Do this based on the Timestamp (date)
+
+	if len(info.DailyStatistics) > 0 {
+		skipIndex := 0
+		lowestTimestamp := info.DailyStatistics[0].Date
+		for _, timestamp := range info.DailyStatistics{
+			if timestamp.Date.Before(lowestTimestamp) {
+				lowestTimestamp = timestamp.Date
+			}
+		}
+
+		if skipIndex >= 0 {
+			info.DailyStatistics = append(info.DailyStatistics[:skipIndex], info.DailyStatistics[skipIndex+1:]...)
+		}
+
+		// Sort the array
+		sort.Slice(info.DailyStatistics, func(i, j int) bool {
+			return info.DailyStatistics[i].Date.Before(info.DailyStatistics[j].Date)
+		})
+
+		// Get a max of the last 60 days
+		if len(info.DailyStatistics) > 60 {
+			info.DailyStatistics = info.DailyStatistics[len(info.DailyStatistics)-60:]
+		}
 	}
 
 	newjson, err := json.Marshal(info)
