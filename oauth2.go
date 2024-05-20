bb0f1e5e8f3b7551166dca711e7bf1724064617d
lalit <lalitdeore12@gmail.com>
2024-05-20 19:02:43 +0530

    sso_backend

--- oauth2.go
+++ oauth2.go
@@ -4080,19 +4080,32 @@ func VerifyIdToken(ctx context.Context, idToken string) (IdTokenCheck, error) {
 			foundOrg := ""
 			foundChallenge := ""
 			stateSplit := strings.Split(string(parsedState), "&")
+			regexPattern := `EXTRA string=([A-Za-z0-9~.]+)`
+			re := regexp.MustCompile(regexPattern)
 			for _, innerstate := range stateSplit {
-				itemsplit := strings.Split(innerstate, "=")
+				itemsplit := strings.SplitN(innerstate, "=", 2)
 				if len(itemsplit) <= 1 {
 					log.Printf("[WARNING] No key:value: %s", innerstate)
 					continue
 				}
 
+				key := strings.TrimSpace(itemsplit[0])
+				value := strings.TrimSpace(itemsplit[1])
 				if itemsplit[0] == "org" {
-					foundOrg = strings.TrimSpace(itemsplit[1])
+					foundOrg = value
 				}
 
-				if itemsplit[0] == "challenge" {
-					foundChallenge = strings.TrimSpace(itemsplit[1])
+				if key == "challenge" {
+					// Extract the "extra string" value from the challenge value
+					matches := re.FindStringSubmatch(value)
+					if len(matches) > 1 {
+						extractedString := matches[1]
+						foundChallenge = extractedString
+						log.Printf("Extracted 'extra string' value is: %s", extractedString)
+					} else {
+						foundChallenge = strings.TrimSpace(itemsplit[1])
+						log.Printf("No 'extra string' value found in challenge: %s", value)
+					}
 				}
 			}
 
@@ -4100,13 +4113,11 @@ func VerifyIdToken(ctx context.Context, idToken string) (IdTokenCheck, error) {
 				log.Printf("[ERROR] No org specified in state (2)")
 				return IdTokenCheck{}, err
 			}
-
 			org, err := GetOrg(ctx, foundOrg)
 			if err != nil {
 				log.Printf("[WARNING] Error getting org in OpenID (2): %s", err)
 				return IdTokenCheck{}, err
 			}
-
 			// Validating the user itself
 			if token.Aud == org.SSOConfig.OpenIdClientId && foundChallenge == org.SSOConfig.OpenIdClientSecret {
 				log.Printf("[DEBUG] Correct token aud & challenge - successful login!")
