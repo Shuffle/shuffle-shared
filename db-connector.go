05b14fe31d5fdabecb91fdb78c5907d4d4103e55
yashsinghcodes <yash9vardhan@gmail.com>
2026-04-01 19:14:25 +0530

    fix: suborg cache check

--- db-connector.go
+++ db-connector.go
@@ -15163,6 +15163,13 @@ func GetDatastoreKey(ctx context.Context, id string, category string) (*CacheKey
 								cacheData = &cacheKey
 								break
 							}
+							
+							for _, subOrg := range cacheKey.SuborgDistribution {
+								if subOrg == orgId {
+									cacheData = &cacheKey
+									break
+								}
+							}
 						}
 
 						if cacheData.Key == "" {
