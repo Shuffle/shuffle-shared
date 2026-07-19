5c04ba76cb1f165478ccc70a97c818c78f78a33a
Hari Krishna <hari.100.hks@outlook.com>
2026-07-19 20:56:42 +0530

    added app description to the actions struct

--- structs.go
+++ structs.go
@@ -5732,9 +5732,10 @@ type ActionSummary struct {
 
 // AppActionResponse - actions grouped by app
 type AppActionResponse struct {
-	AppName string           `json:"app_name"`
-	AppID   string           `json:"app_id"`
-	Actions []ActionSummary  `json:"actions"`
+	AppName        string           `json:"app_name"`
+	AppDescription string           `json:"app_description"`
+	AppID          string           `json:"app_id"`
+	Actions        []ActionSummary  `json:"actions"`
 }
 
 
