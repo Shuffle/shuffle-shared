a633685b42db72274c2fd67e8c884a2b71d0e097
monilprajapati <monillprajapatii@gmail.com>
2026-09-08 20:22:15 +0530

    stream url fix

--- shared.go
+++ shared.go
@@ -38442,16 +38442,24 @@ func streamWorkflowOperations(ctx context.Context, request *http.Request, workfl
 		return
 	}
 
-	baseURL := os.Getenv("BASE_URL")
-	if len(baseURL) == 0 {
-		if len(os.Getenv("SHUFFLE_CLOUDRUN_URL")) > 0 {
-			baseURL = os.Getenv("SHUFFLE_CLOUDRUN_URL")
-		} else {
-			port := os.Getenv("PORT")
-			if len(port) == 0 {
-				port = "5001"
+	var baseURL string
+	if len(os.Getenv("SHUFFLE_GCEPROJECT")) > 0 {
+		baseURL = os.Getenv("SHUFFLE_STREAM_URL")
+		if len(baseURL) == 0 {
+			baseURL = "https://stream.shuffler.io"
+		}
+	} else {
+		baseURL = os.Getenv("BASE_URL")
+		if len(baseURL) == 0 {
+			if len(os.Getenv("SHUFFLE_CLOUDRUN_URL")) > 0 {
+				baseURL = os.Getenv("SHUFFLE_CLOUDRUN_URL")
+			} else {
+				port := os.Getenv("PORT")
+				if len(port) == 0 {
+					port = "5001"
+				}
+				baseURL = fmt.Sprintf("http://localhost:%s", port)
 			}
-			baseURL = fmt.Sprintf("http://localhost:%s", port)
 		}
 	}
 
