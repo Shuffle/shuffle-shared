19ea219f3e9dfd7641236e63500adfadb483d66f
Lalit Deore <lalitdeore12@gmail.com>
2026-09-22 00:34:20 +0530

    add better logging

--- shared.go
+++ shared.go
@@ -16787,9 +16787,9 @@ func HandleGenerateProvisionUrl(resp http.ResponseWriter, request *http.Request)
 			return
 		}
 
-		log.Printf("[WARNING] User %s already exists but wasn't provisioned by org %s", provisionRequest.Email, org.Id)
+		log.Printf("[WARNING] User %s doesn't in org (%v) but was provisioned by org %s", org.Id, provisionRequest.Email, user.ProvisionedByOrg)
 		resp.WriteHeader(409)
-		resp.Write([]byte(`{"success": false, "reason": "User already exists"}`))
+		resp.Write([]byte(`{"success": false, "reason": "User doesn't exist in this org"}`))
 		return
 	}
 
